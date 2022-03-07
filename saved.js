c5318030fc5331d5bead831c700f9936f4e03514
pilot Tobi <tobi@tangleswap.exchange>
2022-03-07 01:54:14 +0100

    add netlify

--- saved.js
+++ saved.js
@@ -1,3 +1,11 @@
+//netlify
+
+// "build": "cross-env NODE_ENV=production remix build",
+// "dev": "cross-env NODE_ENV=development remix dev",
+// "postinstall": "remix setup node",
+// "start": "cross-env NODE_ENV=production remix-serve build"
+
+//remix
 // "build": "remix build",
 // "dev": "remix dev",
 // "postinstall": "remix setup node",
